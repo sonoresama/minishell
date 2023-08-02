75cb6bdadb1a3c8b44f78634fa2e4ff1f8b3bf61
Lerousseau <bastien.lerousseau@gmail.com>
2023-08-02 16:26:42 +0200

    02/08

--- src/check_path.c
+++ src/check_path.c
@@ -6,7 +6,7 @@
 /*   By: blerouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 16:47:25 by blerouss          #+#    #+#             */
-/*   Updated: 2023/07/22 13:26:49 by blerouss         ###   ########.fr       */
+/*   Updated: 2023/08/02 15:08:31 by bastien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,11 @@ char	*path_cmd(char *cmd_name, t_shell *shell, int i)
 	char	**path;
 	char	*path_cmd;
 
-	if (!cmd_name)
-		return (NULL);
-	if (!access(cmd_name, F_OK | X_OK) || cmd_name[0] == '\0')
+	if (!cmd_name || !cmd_name[0])
 		return (ft_strdup(cmd_name));
 	path = parsing_path(shell);
 	if (!path)
-		return (NULL);
+		return (ft_strdup(cmd_name));
 	while (path[++i])
 	{
 		path_cmd = join_three(path[i], "/", cmd_name);
